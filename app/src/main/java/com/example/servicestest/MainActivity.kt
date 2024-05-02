df64cf4a8cd9977d64f68110bee82619ca3a2257
DanilUltrakill <yaremakadanil@gmail.com>
2024-05-03 02:26:44 +0700

    Job Service (scheduler, enqueue)

--- app/src/main/java/com/example/servicestest/MainActivity.kt
+++ app/src/main/java/com/example/servicestest/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.servicestest
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.app.job.JobWorkItem
+import android.content.ComponentName
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -13,6 +19,8 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private var page = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +44,23 @@ class MainActivity : AppCompatActivity() {
             ContextCompat.startForegroundService(
                 this,
                 MyIntentService.newIntent(this))
+        }
+        binding.jobScheduler.setOnClickListener {
+            Log.d("MainActivity", "jobScheduler.setOnClickListener")
+            val componentName = ComponentName(this, MyJobService::class.java)
+
+            val jobInfo = JobInfo.Builder(MyJobService.JOB_ID, componentName)
+                .setRequiresCharging(true)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+                .setPersisted(false)
+                .build()
+
+            val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val intent = MyJobService.newIntent(page++)
+                jobScheduler.enqueue(jobInfo, JobWorkItem(intent))
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/servicestest/MyJobService.kt
@@ -0,0 +1,75 @@
+package com.example.servicestest
+
+import android.app.Service
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import android.os.Message
+import android.os.PersistableBundle
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MyJobService : JobService() {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        coroutineScope.cancel()
+        log("onDestroy")
+    }
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        log("onCommand")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            coroutineScope.launch {
+                var workItem = params?.dequeueWork()
+                while (workItem != null) {
+                    val page = workItem.intent?.getIntExtra(PAGE, 0)
+
+                    for (i in 0 until 5) {
+                        delay(1000)
+                        log("Timer $i $page")
+                    }
+                    params?.completeWork(workItem)
+                    workItem = params?.dequeueWork()
+                }
+                jobFinished(params, false)
+            }
+        }
+        return true
+    }
+
+    override fun onStopJob(params: JobParameters?): Boolean {
+        log("onStopJob")
+        return true
+    }
+
+    private fun log(message: String) {
+        Log.d("SERVICE_TAG", "MyJobService: $message")
+    }
+
+    companion object {
+        const val JOB_ID = 1
+        private const val PAGE = "page"
+
+        fun newIntent(page: Int): Intent {
+            return Intent().apply {
+                putExtra(PAGE, page)
+            }
+        }
+    }
+
+}
